126e5ed49e8db9a10ad9f747797c6ee31ed90064
Lorenzo Gaspari <60689564+lore1986@users.noreply.github.com>
2024-02-08 08:05:13 +0100

    testing action

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-client/"
+  base: "/vite-client/",
+  server: {
+    proxy: {
+      '^/service': {
+          target: 'https://fasito.net',
+          secure: false,
+          changeOrigin: true
+      },
+    }
+  }
 })
